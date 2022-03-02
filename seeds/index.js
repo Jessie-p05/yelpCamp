1c90fe20d6a68eda6b946a61a831868776b73c99
Jessie-p05 <jxpei05@gmail.com>
2022-03-01 23:52:08 -0700

    seed fake campground data into mongoose

---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground'); 
+const cities = require('./cities');
+const {descriptors, places} = require ('./seedHelpers');
+
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+});
+
+const db = mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",() => {
+  console.log("Database connected")
+});
+
+const randomEleOfArray = array => array[Math.floor(Math.random()*array.length)];
+
+
+const seedDB = async() => {
+  await Campground.deleteMany({});
+  for(let i = 0; i < 50; i++) {
+    const randomNum = Math.floor(Math.random()*1000);
+    const camp = new Campground({
+      title:`${randomEleOfArray(descriptors)} ${randomEleOfArray(places)}`,
+      location: `${cities[randomNum].city}, ${cities[randomNum].state}`
+    })
+    await camp.save();
+  }
+
+};
+seedDB();

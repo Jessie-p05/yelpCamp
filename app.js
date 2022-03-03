730d49d7b42386adcd76284752f55ab04f0bb60a
Jessie-p05 <jxpei05@gmail.com>
2022-03-02 21:36:17 -0700

    form and post route to add new campground

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground'); 
+const bodyParser = require('body-parser');
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -17,7 +18,9 @@ db.once("open",() => {
 
 const app = express();
 app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'views'))
+app.set('views', path.join(__dirname,'views'));
+
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/',(req,res) => {
   res.render("home");
@@ -29,13 +32,23 @@ app.get('/campgrounds',async(req,res) => {
   res.render('campgrounds/index',{campgrounds});
 });
 
+app.get('/campgrounds/new',(req,res) => {
+  res.render('campgrounds/new')
+});
+
+app.post('/campgrounds',async(req,res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get('/campgrounds/:id',async(req,res)=> {
   // console.log(req.params.id)
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground})
 });
 
-// app.get('/campground')
 
 app.listen(3000,() => {
   console.log('yelpCame is listening on port 3000')

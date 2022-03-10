5f7bd74f8185c63c919a87a9f29a093fbf8d3fcf
Jessie-p05 <jxpei05@gmail.com>
2022-03-09 23:36:58 -0700

    seed the random campsite pictures

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async() => {
   await Campground.deleteMany({});
   for(let i = 0; i < 50; i++) {
     const randomNum = Math.floor(Math.random()*1000);
+    const randomPrice = Math.floor(Math.random()*30);
     const camp = new Campground({
       title:`${randomEleOfArray(descriptors)} ${randomEleOfArray(places)}`,
-      location: `${cities[randomNum].city}, ${cities[randomNum].state}`
+      location: `${cities[randomNum].city}, ${cities[randomNum].state}`,
+      image:'http://source.unsplash.com/collection/483251',
+      description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat dolorum enim quae nihil libero reprehenderit eveniet eius quibusdam suscipit culpa, eum est molestias sequi saepe labore quos atque veritatis alias?',
+      price: randomPrice
     })
     await camp.save();
   }

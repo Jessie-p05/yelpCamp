726c65f6f6376ac2a21b3f13be828011ab436ad6
Jessie-p05 <jxpei05@gmail.com>
2022-03-18 21:42:24 -0600

    move campground Joi schema to independent file

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require("path");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const ejsMate = require("ejs-mate");
-const Joi = require("joi");
 const catchAsync = require("./helpers/catchAsync");
 const ExpressError = require("./helpers/ExpressError");
+const {campgroundSchema} = require('./schemas')
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 
@@ -33,15 +33,6 @@ app.get("/", (req, res) => {
 });
 
 const validateCampground = (req, res, next) => {
-  const campgroundSchema = Joi.object({
-    campground: Joi.object({
-      title: Joi.string().required(),
-      price: Joi.number().required().min(0),
-      image: Joi.string().required(),
-      location: Joi.string().required(),
-      description: Joi.string().required(),
-    }).required(),
-  });
   const { error } = campgroundSchema.validate(req.body);
   if (error) {
     const msg = error.details.map((el) => el.message).join(",");
@@ -94,7 +85,7 @@ app.get(
 );
 
 app.put(
-  "/campgrounds/:id",
+  "/campgrounds/:id",validateCampground,
   catchAsync(
     catchAsync(async (req, res) => {
       // console.log(req.params)

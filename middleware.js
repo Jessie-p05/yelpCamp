9cb8567479c5397dc1b9a43779c4b74be1b3679d
Jessie-p05 <jxpei05@gmail.com>
2022-04-05 15:50:06 -0600

    fix small bug

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
-module.exports.isLoggedIn = (req,res) => {
+module.exports.isLoggedIn = (req,res,next) => {
   if(!req.isAuthenticated()){
     req.flash('error','please login to continue!');
-    res.redirect('/login')
+    return res.redirect('/login')
   }
+  next();
 }

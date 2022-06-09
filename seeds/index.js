cbec770ce953fd7a246285f7736e1d82d882fdd3
Jessie-p05 <jxpei05@gmail.com>
2022-06-09 16:13:01 -0600

    seed fake data into mongo atlas

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
 const { descriptors, places } = require("./seedHelpers");
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
